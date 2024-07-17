dbb74228dbe61a7d090706f8fd626c446be1506c
AndressaLp <andressarodrigueslopes157@gmail.com>
2024-07-16 23:16:16 -0300

    exibindo detalhes de edição

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Projects from "./components/pages/Projects";
 import NewProject from "./components/pages/NewProject"
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer"
+import Project from "./components/pages/Project"
 
 function App() {
   return (
@@ -16,10 +17,11 @@ function App() {
         <Container customClass="min_height">
           <Routes>
             <Route exact path="/" element={<Home/>} />
-            <Route exact path="/projects" element={<Projects/>} />
-            <Route exact path="/newproject" element={<NewProject/>} />
-            <Route exact path="/contact" element={<Contact/>} />
-            <Route exact path="/company" element={<Company/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/newproject" element={<NewProject/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/company" element={<Company/>} />
+            <Route path="/project/:id" element={<Project/>} />
           </Routes>
         </Container>
         <Footer/>
